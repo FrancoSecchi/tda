7aa122bc19fa183f3d88f5205a7c6ac7ff3326b2
romeromaxi <mromerov@fi.uba.ar>
2023-11-19 01:00:52 -0300

    Correcciones en Parte 1

--- tp2/arturo/arturo.py
+++ tp2/arturo/arturo.py
@@ -21,6 +21,22 @@ def maxima_subsecuencia_popularidad(popularidades):
     return maximo_calculado, index_fin
 
 
+def maxima_subsecuencia_circular(popularidades):
+    maximo_obtenido, idx_fin = maxima_subsecuencia_popularidad(popularidades)
+
+    total_popularidades = sum(popularidades)
+    popularidades_negadas = [-x for x in popularidades]
+    minimo_obtenido, idx_fin_minimo = maxima_subsecuencia_popularidad(
+        popularidades_negadas
+    )
+    maximo_circular = total_popularidades + minimo_obtenido
+
+    if maximo_obtenido >= maximo_circular:
+        return maximo_obtenido, idx_fin, False
+
+    return maximo_circular, idx_fin_minimo, True
+
+
 def main():
     if len(sys.argv) != 2:
         print("Uso: python arturo.py caballeros.txt")
@@ -35,16 +51,26 @@ def main():
         print("No se han encontrado caballeros")
         return
 
-    maximo_obtenido, idx_fin = maxima_subsecuencia_popularidad(popularidades)
+    maximo_obtenido, idx_fin, max_en_extremos = maxima_subsecuencia_circular(
+        popularidades
+    )
 
+    cantidad_caballeros = len(nombres_caballeros)
     maximo_parcial = 0
     se_llego_suma = False
     i = idx_fin
+
+    if max_en_extremos:
+        i += 1
+
     while not se_llego_suma:
         maximo_parcial += popularidades[i]
         se_llego_suma = maximo_parcial == maximo_obtenido
         print(nombres_caballeros[i], end=", " if not se_llego_suma else "")
-        i -= 1
+
+        i += 1 if max_en_extremos else -1
+
+        i = (i + cantidad_caballeros) % cantidad_caballeros
 
 
 if __name__ == "__main__":
--- tp2/arturo/arturo_utils.py
+++ tp2/arturo/arturo_utils.py
@@ -2,9 +2,6 @@ def leer_caballeros(nombre_archivo):
     nombres_caballeros = []
     popularidades_caballeros = []
 
-    min_popularidad = None
-    index_min_popularidad = 0
-
     try:
         with open(nombre_archivo, "r") as archivo:
             for linea in archivo:
@@ -15,27 +12,11 @@ def leer_caballeros(nombre_archivo):
                 nombres_caballeros.append(nombre_caballero)
                 popularidades_caballeros.append(popularidad_caballero)
 
-                if (min_popularidad is None) or (
-                    popularidad_caballero < min_popularidad
-                ):
-                    min_popularidad = popularidad_caballero
-                    index_min_popularidad = len(popularidades_caballeros) - 1
-
     except FileNotFoundError as e:
         print(f"El archivo '{nombre_archivo}' no fue encontrado.")
         raise e
     except Exception as e:
         print(f"Ocurrió un error al procesar el archivo: {str(e)}")
         raise e
-
-    if index_min_popularidad > 0:
-        nombres_caballeros = (
-            nombres_caballeros[index_min_popularidad:]
-            + nombres_caballeros[:index_min_popularidad]
-        )
-        popularidades_caballeros = (
-            popularidades_caballeros[index_min_popularidad:]
-            + popularidades_caballeros[:index_min_popularidad]
-        )
 
     return nombres_caballeros, popularidades_caballeros
